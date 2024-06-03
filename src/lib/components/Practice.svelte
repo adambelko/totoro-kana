8b09e25260e3fb003df4daeb5fc4351b0531d921
Adam Belko <adam.belko@archa.com.au>
2024-06-04 09:07:13 +1000

    selecting kana based on categories in place for both hiragana and katakana

--- src/lib/components/Practice.svelte
+++ src/lib/components/Practice.svelte
@@ -3,18 +3,33 @@
 	import { hiragana } from "$lib/data/hiragana"
 	import { katakana } from "$lib/data/katakana"
 
-	let selectedGroups: string[] = []
-	$: {
-		console.log(selectedGroups)
+	interface KanaGroup {
+		category: string
+		groupName: string
+		romaji: string
+		japanese: string
 	}
+
+	let hiraganaGroups: KanaGroup[] = []
+	let katakanaGroups: KanaGroup[] = []
+	$: selectedGroups = [{ hiragana: [...hiraganaGroups] }, { katakana: [...katakanaGroups] }]
+	$: console.log(selectedGroups)
 </script>
 
 <div class="mt-6 flex gap-4 bg-white/30 p-8 rounded-container-token dark:bg-black/30">
 	<div class="flex-1">
-		<SelectKanaGroupListBox title="Hiragana · ひらがな" data={hiragana} bind:selectedGroups />
+		<SelectKanaGroupListBox
+			title="Hiragana / ひらがな"
+			data={hiragana}
+			bind:selectedGroups={hiraganaGroups}
+		/>
 	</div>
 	<div class="flex-1">
-		<SelectKanaGroupListBox title="Katakana · カタカナ" data={katakana} bind:selectedGroups />
+		<SelectKanaGroupListBox
+			title="Katakana / カタカナ"
+			data={katakana}
+			bind:selectedGroups={katakanaGroups}
+		/>
 	</div>
 </div>
 <button class="variant-filled-primary btn mx-auto mb-10 mt-4 block">Start practice</button>
--- src/lib/components/SelectKanaGroupListBox.svelte
+++ src/lib/components/SelectKanaGroupListBox.svelte
@@ -1,15 +1,73 @@
 <script lang="ts">
 	import { ListBox, ListBoxItem } from "@skeletonlabs/skeleton"
-	import { formatData } from "$lib/helpers/formatKanaCharacters.js"
+	import { formatData } from "$lib/helpers/formatKanaCharacters"
 
-	export let selectedGroups: string[]
+	interface KanaGroup {
+		category: string
+		groupName: string
+		romaji: string
+		japanese: string
+	}
+
+	export let selectedGroups: KanaGroup[]
 	export let title: string
 	export let data
 
-	const formattedData = formatData(data)
+	const listBoxData = formatData(data)
 
-	$: {
-		console.log(formattedData)
+	let allChecked = false
+	let mainChecked = false
+	let dakutenChecked = false
+	let combinationChecked = false
+
+	function handleAllChecked() {
+		if (allChecked) {
+			mainChecked = true
+			dakutenChecked = true
+			combinationChecked = true
+			selectedGroups = listBoxData.slice()
+		} else {
+			mainChecked = false
+			dakutenChecked = false
+			combinationChecked = false
+			selectedGroups = []
+		}
+	}
+
+	function handleMainChecked() {
+		const mainGroups = listBoxData.filter((kanaGroup) => kanaGroup.category === "mainKana")
+		if (mainChecked) {
+			selectedGroups = Array.from(new Set([...selectedGroups, ...mainGroups]))
+		} else {
+			selectedGroups = selectedGroups.filter((group) => !mainGroups.includes(group))
+		}
+		updateAllChecked()
+	}
+
+	function handleDakutenChecked() {
+		const dakutenGroups = listBoxData.filter((kanaGroup) => kanaGroup.category === "dakutenKana")
+		if (dakutenChecked) {
+			selectedGroups = Array.from(new Set([...selectedGroups, ...dakutenGroups]))
+		} else {
+			selectedGroups = selectedGroups.filter((group) => !dakutenGroups.includes(group))
+		}
+		updateAllChecked()
+	}
+
+	function handleCombinationChecked() {
+		const combinationGroups = listBoxData.filter(
+			(kanaGroup) => kanaGroup.category === "combinationKana"
+		)
+		if (combinationChecked) {
+			selectedGroups = Array.from(new Set([...selectedGroups, ...combinationGroups]))
+		} else {
+			selectedGroups = selectedGroups.filter((group) => !combinationGroups.includes(group))
+		}
+		updateAllChecked()
+	}
+
+	function updateAllChecked() {
+		allChecked = mainChecked && dakutenChecked && combinationChecked
 	}
 </script>
 
@@ -22,20 +80,48 @@
 	hover="hover:variant-soft-primary"
 	class="flex-1 bg-surface-200"
 >
-	{#each formattedData as kanaGroup}
+	{#each listBoxData as kanaGroup}
 		<ListBoxItem bind:group={selectedGroups} name="medium" value={kanaGroup}>
 			{kanaGroup.romaji} / {kanaGroup.japanese}
 		</ListBoxItem>
 	{/each}
 </ListBox>
 
-<div class="mt-2 flex justify-center gap-6">
+<div class="mt-2 flex justify-center gap-4">
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" />
+		<input
+			class="checkbox"
+			type="checkbox"
+			bind:checked={allChecked}
+			on:change={handleAllChecked}
+		/>
 		<span>All</span>
 	</label>
 	<label class="flex items-center space-x-2">
-		<input class="checkbox" type="checkbox" />
-		<span>Alternative</span>
+		<input
+			class="checkbox"
+			type="checkbox"
+			bind:checked={mainChecked}
+			on:change={handleMainChecked}
+		/>
+		<span>Main</span>
+	</label>
+	<label class="flex items-center space-x-2">
+		<input
+			class="checkbox"
+			type="checkbox"
+			bind:checked={dakutenChecked}
+			on:change={handleDakutenChecked}
+		/>
+		<span>Dakuten</span>
+	</label>
+	<label class="flex items-center space-x-2">
+		<input
+			class="checkbox"
+			type="checkbox"
+			bind:checked={combinationChecked}
+			on:change={handleCombinationChecked}
+		/>
+		<span>Combination</span>
 	</label>
 </div>
