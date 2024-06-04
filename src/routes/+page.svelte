9daad09da31c17576ead1cbd74dfe7cab56c7026
Adam Belko <adam.belko@archa.com.au>
2024-06-04 10:04:21 +1000

    move Practice.svelte to root +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
+	import SelectKanaGroupListBox from "$lib/components/SelectKanaGroupListBox.svelte"
 	import { AppBar } from "@skeletonlabs/skeleton"
-	import KanaListBox from "$lib/components/Practice.svelte"
+	import { hiragana } from "$lib/data/hiragana"
+	import { katakana } from "$lib/data/katakana"
+
+	interface KanaGroup {
+		category: string
+		groupName: string
+		romaji: string
+		japanese: string
+	}
+
+	let hiraganaGroups: KanaGroup[] = []
+	let katakanaGroups: KanaGroup[] = []
+	$: selectedGroups = [{ hiragana: [...hiraganaGroups] }, { katakana: [...katakanaGroups] }]
+	$: console.log(selectedGroups)
 </script>
 
 <AppBar class="mt-6 p-6 rounded-container-token" background="variant-ghost"
 	>Welcome back user!</AppBar
 >
-<KanaListBox />
+
+<div class="mt-6 flex gap-4 bg-white/30 p-8 rounded-container-token dark:bg-black/30">
+	<div class="flex-1">
+		<SelectKanaGroupListBox
+			title="Hiragana / ひらがな"
+			data={hiragana}
+			bind:selectedGroups={hiraganaGroups}
+		/>
+	</div>
+	<div class="flex-1">
+		<SelectKanaGroupListBox
+			title="Katakana / カタカナ"
+			data={katakana}
+			bind:selectedGroups={katakanaGroups}
+		/>
+	</div>
+</div>
+<button class="variant-filled-primary btn mx-auto mb-10 mt-4 block">Start practice</button>
