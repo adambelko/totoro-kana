308339809b395c74a2269ba3fb2fd589662f78f2
Adam Belko <adam.belko@archa.com.au>
2024-07-01 12:57:28 +1000

    add user to db

--- src/routes/study/+page.server.ts
+++ src/routes/study/+page.server.ts
@@ -1,6 +1,30 @@
+import type { ServerLoad } from "@sveltejs/kit"
 import { db } from "$lib/db"
+import { users } from "$lib/db/schema"
+import { eq } from "drizzle-orm"
+import type { Session } from "@supabase/supabase-js"
 
-export const load = async () => {
-	const users = await db.query.users.findMany()
-	return { users }
+export const load: ServerLoad = async ({ locals }) => {
+	const { session } = await locals.safeGetSession()
+	const email = session?.user?.email
+	const userData = email && (await db.select().from(users).where(eq(users.email, email)))
+
+	if (!userData || userData.length === 0) {
+		await saveUser(session)
+	}
+}
+
+const saveUser = async (session: Session | null) => {
+	const email = session?.user?.email
+	const userData = email ? await db.select().from(users).where(eq(users.email, email)) : []
+
+	if (userData.length === 0 && email) {
+		await db
+			.insert(users)
+			.values({
+				id: crypto.randomUUID(),
+				email: email
+			})
+			.returning()
+	}
 }
