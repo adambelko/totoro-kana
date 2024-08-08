9e1d85d9fc2fda37142f8e49ef6e73dfd269cc61
Adam Belko <adam.belko@archa.com.au>
2024-08-09 09:25:50 +1000

    page error handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from "$app/stores"
+	import { goto } from "$app/navigation"
+	import { onMount } from "svelte"
+
+	export let data
+	const { session } = data
+
+	onMount(() => {
+		setTimeout(() => {
+			session ? goto("/learn/hiragana") : goto("/")
+		}, 2000)
+	})
+</script>
+
+<svelte:head>
+	<title>{$page.status}</title>
+</svelte:head>
+
+<div class="mt-4 flex justify-center bg-white/30 p-6 rounded-container-token">
+	{#if $page.status === 404}
+		<h2 class="h2">Not found</h2>
+	{:else}
+		<h2 class="h2">Something went wrong</h2>
+	{/if}
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,11 @@
 <div class="mb-6 mt-4 flex flex-col bg-white/30 p-6 rounded-container-token dark:bg-black/30">
 	<h2 class="h2 p-4 text-center">Login</h2>
 	<div class="flex flex-col gap-4 text-center">
-		<a href="/auth/login?provider=github">
-			<button class="variant-filled-primary btn">Github</button>
-		</a>
 		<a href="/auth/login?provider=spotify">
 			<button class="variant-filled-primary btn">Spotify</button>
+		</a>
+		<a href="/auth/login?provider=github">
+			<button class="variant-filled-primary btn">Github</button>
 		</a>
 	</div>
 </div>
