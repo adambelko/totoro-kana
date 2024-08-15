e5776e2ac62b6b9c76fc685f35ebc1ee86c061ab
Adam Belko <adam.belko@archa.com.au>
2024-08-16 07:00:19 +1000

    navigate logged in users to home page upon visiting login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,11 +1,22 @@
+<script lang="ts">
+  import {goto} from "$app/navigation"
+  import {onMount} from "svelte"
+
+  export let data
+
+  onMount(() => {
+    if (data.user) goto("/")
+  })
+</script>
+
 <div class="mt-4 flex flex-col bg-white/30 p-6 rounded-container-token">
-	<h2 class="h2 p-4 text-center">Sing in</h2>
-	<div class="flex flex-col gap-4 text-center">
-		<a href="/auth/login?provider=spotify">
-			<button class="bg-tertiary-400 btn">Continue with<span class="font-bold">&nbsp;Spotify</span></button>
-		</a>
-		<a href="/auth/login?provider=github">
-			<button class="bg-tertiary-500 btn">Continue with<span class="font-bold">&nbsp;GitHub</span></button>
-		</a>
-	</div>
+    <h2 class="h2 p-4 text-center">Sing in</h2>
+    <div class="flex flex-col gap-4 text-center">
+        <a href="/auth/login?provider=spotify">
+            <button class="bg-tertiary-400 btn">Continue with<span class="font-bold">&nbsp;Spotify</span></button>
+        </a>
+        <a href="/auth/login?provider=github">
+            <button class="bg-tertiary-500 btn">Continue with<span class="font-bold">&nbsp;GitHub</span></button>
+        </a>
+    </div>
 </div>
