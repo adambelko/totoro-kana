58a32eac8cd0b6cfd4848722e3686b3a59c0c0bd
Adam Belko <adam.belko@archa.com.au>
2024-08-27 06:11:04 +1000

    install floating ui for popups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import { invalidate } from "$app/navigation"
 	import { onMount } from "svelte"
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
 	import Nav from "$lib/components/Nav.svelte"
 	import Footer from "$lib/components/Footer.svelte"
 	import "../app.css"
 
 	export let data
 	$: ({ session, supabase } = data)
+
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow })
 
 	onMount(() => {
 		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
