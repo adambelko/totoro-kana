21296b7142c8b66982b91d264b728dfc91a515d9
Adam Belko <adam.belko@archa.com.au>
2024-08-28 08:14:30 +1000

    add Google OAuth

--- src/routes/auth/login/+server.ts
+++ src/routes/auth/login/+server.ts
@@ -3,14 +3,31 @@ import { redirect } from "@sveltejs/kit"
 
 export const GET = async ({ locals, url }) => {
 	const provider = url.searchParams.get("provider") as Provider
-	if (!provider) throw redirect(307, "/login")
+	if (!provider) {
+		throw redirect(307, "/login")
+	}
 
-	const { data } = await locals.supabase.auth.signInWithOAuth({
+	const oauthOptions: {
+		provider: Provider
+		options: {
+			redirectTo: string
+			queryParams?: Record<string, string>
+		}
+	} = {
 		provider,
 		options: {
-			redirectTo: url.origin + "/auth/callback"
+			redirectTo: `${url.origin}/auth/callback`
 		}
-	})
+	}
+
+	if (provider === "google") {
+		oauthOptions.options.queryParams = {
+			access_type: "offline",
+			prompt: "consent"
+		}
+	}
+
+	const { data } = await locals.supabase.auth.signInWithOAuth(oauthOptions)
 
 	if (data.url) {
 		throw redirect(307, data.url)
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -17,6 +17,14 @@
 <div class="mt-4 flex flex-col bg-white/30 pt-6 pb-10 rounded-container-token">
     <h2 class="h2 p-4 text-center">Log in</h2>
     <div class="flex flex-col gap-4 items-center">
+        <a href="/auth/login?provider=google">
+            <button class="bg-tertiary-400 btn">
+                <Icon icon="ri:google-fill" width="1.3em" height="1.3em"  style="color: black" />
+                <div class="pr-2">
+                    Continue with &nbsp;<span class="font-bold">Google</span>
+                </div>
+            </button>
+        </a>
         <a href="/auth/login?provider=spotify">
             <button class="bg-primary-400 btn">
                 <Icon icon="mdi:spotify" width="1.3em" height="1.3em" style="color: black" />
